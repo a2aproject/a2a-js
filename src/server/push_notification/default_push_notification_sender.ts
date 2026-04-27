e7086df1b589e88cb18265f7976ab06a007d2896
Bartek Gralewicz <bgralewicz@google.com>
2026-04-27 13:18:21 +0000

    Revert "Update src/server/push_notification/default_push_notification_sender.ts"
    This reverts commit 5395a99bc188eb11abc5167effdf684b1c432882.

--- src/server/push_notification/default_push_notification_sender.ts
+++ src/server/push_notification/default_push_notification_sender.ts
@@ -105,11 +105,11 @@ export class DefaultPushNotificationSender implements PushNotificationSender {
    */
   private _buildAuthHeaders(pushConfig: TaskPushNotificationConfig): Record<string, string> {
     const headers: Record<string, string> = { 'Content-Type': 'application/json' };
-    const auth = pushConfig.authentication;
 
-    if (auth?.scheme != null && auth?.credentials != null) {
-      headers['Authorization'] = auth.scheme + ' ' + auth.credentials;
-    } else if (pushConfig.token != null) {
+    if (pushConfig.authentication?.scheme && pushConfig.authentication?.credentials) {
+      headers['Authorization'] =
+        `${pushConfig.authentication.scheme} ${pushConfig.authentication.credentials}`;
+    } else if (pushConfig.token) {
       headers[this.options.tokenHeaderName] = pushConfig.token;
     }
 
