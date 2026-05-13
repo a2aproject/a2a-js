aeae71f65340885d22652b93f1b812df08d588e4
JakubWorek <worekkuba@gmail.com>
2026-05-13 14:53:19 +0000

    #453 fix: exclude created types files from code coverage check

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,14 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'json', 'html', 'json-summary'],
       include: ['src/**/*'],
-      exclude: ['src/samples/**/*', 'src/types/pb/**/*', 'src/grpc/pb/**/*'],
+      exclude: [
+        'src/samples/**/*',
+        'src/types/pb/**/*',
+        'src/grpc/pb/**/*',
+        'src/compat/v0_3/types/pb/**/*',
+        'src/compat/v0_3/types/types.ts',
+        'src/compat/v0_3/types/rest_types.ts',
+      ],
     },
   },
 });
